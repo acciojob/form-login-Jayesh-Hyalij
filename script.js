ffbba9453e3c69cd51edde75863bea7396370dd1
Jayesh Hyalij <115018197+Jayesh-Hyalij@users.noreply.github.com>
2024-06-11 19:58:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
-    //Write your code here
-
+	let inp = document.querySelectorAll("input")
+	let first_name = inp[0].value
+	let last_name = inp[1].value
+	let full_name = first_name + " " + last_name
+	alert(full_name)
 }
